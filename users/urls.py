8455fc8837fbcf425c83e73ca65f791586a693c6
Ty <tyjanik44@gmail.com>
2021-07-29 21:22:49 -0500

    created api view to get user by username

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import GetUser
 
 urlpatterns = [
-
+    path('get-user', GetUser.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import AccountSerializer
+from .models import Account
 
-# Create your views here.
+
+class GetUser(APIView):  # gets a user by username
+    serializer_class = AccountSerializer
+    lookup_url_kwarg = 'username'
+
+    def get(self, request, format=None):
+        username = request.GET.get(self.lookup_url_kwarg)
+        if username is not None:
+            user = Account.objects.filter(username=username)
+            if len(user) > 0:
+                data = AccountSerializer(user[0]).data  # take the first occurrence since there should only be one (username is unique)
+                return Response(data, status=status.HTTP_200_OK)
+            return Response(
+                {'User not found': 'Invalid username'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        return Response(
+            {'Bad Request': 'Username not found in request'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
