2ce8b483e284e1e61f5338e643ef07390cf4ca75
tjanik7 <tyjanik44@gmail.com>
2021-11-13 17:14:07 -0600

    added favicon

--- vynle_app/urls.py
+++ vynle_app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('', include('frontend.urls')),  # frontend urls to be dispatched with react router
@@ -7,4 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('spotify/', include('spotify.urls')),
+    # Tells server to look in /static/img/favicon.ico for icon
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
