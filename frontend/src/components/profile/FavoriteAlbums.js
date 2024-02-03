76b9a9bf1feed20a10b5019345eb1a449bed66d0
Ty Janik <tyjanik44@gmail.com>
2024-02-02 23:23:48 -0500

    finally resolved issues; need to cleanup code and figure out what to add to other branch

--- frontend/src/components/profile/FavoriteAlbums.js
+++ frontend/src/components/profile/FavoriteAlbums.js
@@ -6,13 +6,14 @@ import Search from "../search/Search"
 import { setSelectedIndex } from "../../actions/spotifySearch"
 import { connect } from "react-redux"
 import { setFavAlbum } from "../../actions/profile"
+import './css/FavoriteAlbums.css'
 
 function generateAlbumTags(props, setSearchDisplayed, isClickable) {
     const columns = []
 
     for (let i = 0; i < 6; i++) {  // Generates JSX tags for album art
         columns.push(
-            <Col key={i}>
+            <Col className={'tst-col'} key={i}>
                 <CoverArt
                     albumData={props.profile.favoriteAlbums[i]}
                     isClickable={isClickable}
@@ -44,7 +45,7 @@ function FavoriteAlbums(props) {
     return (
         <>
             <Container>
-                <Row xs={6}>
+                <Row className={'tst-row'} xs={6}>
                     {generateAlbumTags(props, setSearchDisplayed, isProfileOwner)}
                 </Row>
             </Container>
