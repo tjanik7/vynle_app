284488b0c707b9b20d6b280dd43a2381ff212693
Ty Janik <tyjanik44@gmail.com>
2025-02-10 17:57:45 -0500

    Server locations are now specified as env vars

--- frontend/src/api/serverLocations.js
+++ frontend/src/api/serverLocations.js
@@ -1,5 +1,6 @@
-export const backendServerLocation = 'https://localhost:444'
-const frontendServerLocation = 'https://localhost'
+// TODO: toggle these based on config
+export const backendServerLocation = process.env.NODE_ENV === 'production' ? 'https://api.vynle.com' : 'http://localhost:8000'
+const frontendServerLocation = process.env.NODE_ENV === 'production' ? 'https://vynle.com' : 'http://localhost'
 
 export const buildStaticUrl = (file) => { // Points to frontend server location
     return frontendServerLocation + '/static/' + file
